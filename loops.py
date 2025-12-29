57f0fd23535661dc7b4d31f12b43479507ce4d3c
neeraja0618 <kovuruneeraja2@gmail.com>
2025-12-29 21:31:32 +0530

    Added loop practice

---
+++ loops.py
@@ -0,0 +1,9 @@
+#printing num as per user input 
+num=int(input("enter the last number"))
+for i in range(1,num):
+          print("number:",i)
+#sum of all numbers
+n=num-1
+print("sum:",n*(n+1)//2)
+
+                
